0d9f6019c00879a4f6d71f628aafa3574957543d
Jon Crain <joncrain@gmail.com>
2020-07-07 16:11:02 -0400

    Fixing tab view

--- views/nomad_tab.php
+++ views/nomad_tab.php
@@ -8,6 +8,10 @@ $(document).on('appReady', function(){
     $.getJSON(appUrl + '/module/nomad/get_data/' + serialNumber, function(data){
         var table = $('#nomad-tab-table');
         $.each(data, function(key,val){
+            if (key == "nomad_enabled" || key == "nomad_login_enabled" || key == "use_keychain" || key == "local_password_sync") {
+                val = val == 1 ? i18n.t('yes') :
+         		(val == 0 && val != null ? i18n.t('no') : '')
+            }
             var th = $('<th>').text(i18n.t('nomad.column.' + key));
             var td = $('<td>').text(val);
             table.append($('<tr>').append(th, td));
