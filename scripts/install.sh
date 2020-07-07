068248aef35e7c21fa8b8d1afe91ed8fd6234e57
Jon Crain <joncrain@gmail.com>
2020-07-07 15:35:34 -0400

    Adding script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,19 +4,19 @@
 CTL="${BASEURL}index.php?/module/nomad/"
 
 # Get the scripts in the proper directories
-"${CURL[@]}" "${CTL}get_script/nomad.sh" -o "${MUNKIPATH}preflight.d/nomad.sh"
+"${CURL[@]}" "${CTL}get_script/nomad" -o "${MUNKIPATH}preflight.d/nomad"
 
 # Check exit status of curl
 if [ $? = 0 ]; then
 	# Make executable
-	chmod a+x "${MUNKIPATH}preflight.d/nomad.sh"
+	chmod a+x "${MUNKIPATH}preflight.d/nomad"
 
 	# Set preference to include this file in the preflight check
 	setreportpref "nomad" "${CACHEPATH}nomad.txt"
 
 else
 	echo "Failed to download all required components!"
-	rm -f "${MUNKIPATH}preflight.d/nomad.sh"
+	rm -f "${MUNKIPATH}preflight.d/nomad"
 
 	# Signal that we had an error
 	ERR=1
