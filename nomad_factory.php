4021b69027bd19277724158f0d2e9d2cb625b699
Jon Crain <joncrain@gmail.com>
2020-07-08 11:51:31 -0400

    Updating factory defaults and cleaning up processor

--- nomad_factory.php
+++ nomad_factory.php
@@ -7,13 +7,13 @@
 $factory->define(Nomad_model::class, function (Faker\Generator $faker) {
 
     return [
-        'nomad_version' => $faker->word(),
-        'nomad_login_version' => $faker->word(),
+        'nomad_version' => $faker->numerify('1.#.#'),
+        'nomad_login_version' => $faker->numerify('1.#.#'),
         'nomad_enabled' => $faker->boolean(),
         'nomad_login_enabled' => $faker->boolean(),
-        'nomad_user' => $faker->word(),
-        'addomain' => $faker->word(),
-        'kerberos_realm' => $faker->word(),
+        'nomad_user' => $faker->firstName(),
+        'addomain' => $faker->domainName(),
+        'kerberos_realm' => $faker->domainName(),
         'use_keychain' => $faker->boolean(),
         'local_password_sync' => $faker->boolean(),
     ];
--- nomad_processor.php
+++ nomad_processor.php
@@ -6,23 +6,17 @@ class Nomad_processor extends Processor
 {
     public function run($json)
     {
-        $modelData = ['serial_number' => $this->serial_number];
-
         // Check if data was uploaded
         if (! $json ) {
             throw new Exception("Error Processing Request: No JSON file found", 1);
         }
         // Process json into object thingy
         $data = json_decode($json, true);
-
-		foreach($data as $key=>$val) {
-            $modelData[$key] = $val;
-		} //end foreach explode lines
-
+        $data['serial_number'] = $this->serial_number;
         Nomad_model::updateOrCreate(
-            ['serial_number' => $this->serial_number], $modelData
+			['serial_number' => $this->serial_number],
+            $data
         );
-        
         return $this;
-    }   
+    }  
 }
